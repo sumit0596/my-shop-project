f841e3cdb8627dcd024a50b831d751ed19f02be9
sumit0596 <sumit.mathur@exazeit.com>
2026-05-18 20:46:22 +0530

    product changes v2

--- src/common/db/mysql.js
+++ src/common/db/mysql.js
@@ -2,12 +2,12 @@ const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
   host: 'localhost',
-  user: 'root',
-  password: '',
+  user: 'phpmyadmin',
+  password: 'Myhome@12345Server',
   database: 'ecommerce',
   waitForConnections: true,
   connectionLimit: 10,
-  port: 3307  
+  port: 3306  
 });
 
 module.exports = pool;
